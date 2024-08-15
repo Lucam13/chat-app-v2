6d17a121292d238f56eb0ed7be8dcf01fc7b7dd6
Dante <denicolodante@gmail.com>
2024-08-15 00:55:53 -0300

    fixed unique key for messages

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -16,21 +16,22 @@ const Messages = () => {
 	}, [messages]);
 
 	return (
-		<div className='px-4 flex-1 overflow-auto'>
+		<div className="px-4 flex-1 overflow-auto">
 			{!loading &&
 				messages.length > 0 &&
-				messages.map((message) => (
-					<div key={message._id} ref={lastMessageRef}>
+				messages.map((message, index) => (
+					<div key={message._id || index} ref={lastMessageRef}>
 						<Message message={message} />
 					</div>
 				))}
 
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
-				<p className='text-center'>Envia un mensaje para empezar la conversacion!</p>
+				<p className="text-center">
+					Envia un mensaje para empezar la conversacion!
+				</p>
 			)}
 		</div>
 	);
 };
 export default Messages;
-
